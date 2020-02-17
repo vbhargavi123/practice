f964bd5d3efe5f88209b7de5d586bb66b053bdfe
bhargavi123 <59689154+vbhargavi123@users.noreply.github.com>
2020-02-17 17:49:17 +0530

    Add files via upload

---
+++ springboot- hibernate/bootapp-hibernate/src/main/java/com/cts/training/bootapphibernate/exception/ProductNotFoundException.java
@@ -0,0 +1,9 @@
+package com.cts.training.bootapphibernate.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+	public ProductNotFoundException(String msg) {
+		// TODO Auto-generated constructor stub
+	super(msg);
+	}
+
+}
